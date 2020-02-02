5f4edaf68e80310c25f7baf19ab8677b55c06a93
ahmedshaheen <ahmedshaheen676@yahoo.com>
2020-02-02 22:34:10 +0200

    solve maven duplication on dependencies

--- view-server/src/main/java/module-info.java
+++ view-server/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module view.server {
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires model.server;
     requires controller.server;
+    requires model.server;
+    requires javafx.base;
+    requires javafx.fxml;
+    requires javafx.controls;
+
     opens com.chat.server.view.server.controller;
     opens com.chat.server.view.server;
 }
