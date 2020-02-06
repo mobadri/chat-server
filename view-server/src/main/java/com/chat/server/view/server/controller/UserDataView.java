bc017991db5d6e910b3cd63043b4906cac704b67
ahmedshaheen <ahmedshaheen676@yahoo.com>
2020-02-06 23:23:13 +0200

    add maven plugin and config

---
+++ view-server/src/main/java/com/chat/server/view/server/controller/UserDataView.java
@@ -0,0 +1,13 @@
+package com.chat.server.view.server.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserDataView implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
