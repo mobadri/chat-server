1d76dfd46e7469a1419321579a2fdbcdc4807806
Yasmeen2080 <yasmeen2080@gmail.com>
2020-02-12 22:50:53 +0200

    recieve message from client

--- service-server/src/main/java/com/chat/server/service/server/message/ServerMessageService.java
+++ service-server/src/main/java/com/chat/server/service/server/message/ServerMessageService.java
@@ -1,9 +1,17 @@
 package com.chat.server.service.server.message;
 
+import com.chat.server.model.chat.Message;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface ServerMessageService extends Remote {
+    /**
+     * send Message to group
+     * @param message to send
+     */
 
+        void sendMessage(Message message) throws RemoteException;
    /* *//**
      * to register client
      * @param clientMessageService client service to register it
