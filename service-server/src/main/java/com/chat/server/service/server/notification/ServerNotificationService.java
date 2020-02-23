86ee87e34d11577b770043ccff2b0c45c5e47f73
DELL <radwahussien24@gmail.com>
2020-02-23 18:27:13 +0200

    solve bug

--- service-server/src/main/java/com/chat/server/service/server/notification/ServerNotificationService.java
+++ service-server/src/main/java/com/chat/server/service/server/notification/ServerNotificationService.java
@@ -13,24 +13,26 @@ import java.util.List;
 public interface ServerNotificationService extends Remote {
     /**
      * get user notifications which seen or not seen
+     *
      * @param user user to get his notification
      * @param seen seen Or not seen
      * @return list of user notifications
      */
-    public List<Notification> getUserNotification(User user ,boolean seen) throws RemoteException;
+    public List<Notification> getUserNotification(User user, boolean seen) throws RemoteException;
 
     /**
      * get user notification which seen or not by notification type
-     * @param user user to get his notifications
-     * @param seen seen Or not seen
+     *
+     * @param user             user to get his notifications
+     * @param seen             seen Or not seen
      * @param notificationType notification type
      * @return
      */
-    public List<Notification> getUserNotificationByType(User user, boolean seen , NotificationType notificationType) throws RemoteException;
+    public List<Notification> getUserNotificationByType(User user, boolean seen, NotificationType notificationType) throws RemoteException;
 
     void sendNotification(Notification notification) throws RemoteException;
 
-    Notification createChangeModeNotification(User user, Mode mode, User friend);
+    Notification createChangeModeNotification(User user, Mode mode, User friend) throws RemoteException;
 
     void register(NotificationServiceCallback notificationServiceCallback) throws RemoteException;
 
