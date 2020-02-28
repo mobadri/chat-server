d2f7facc661c216c024aeb7d05a90f4838f71ebc
ahmedshaheen <ahmedshaheen676@yahoo.com>
2020-02-28 12:54:18 +0200

    generate jar file

---
+++ view-server/src/main/java/com/chat/server/view/server/App.java
@@ -0,0 +1,30 @@
+package com.chat.server.view.server;
+
+import com.chat.server.controller.server.ServerControllerImpl;
+import com.chat.server.view.server.controller.ServerViewController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/templates/server-view.fxml"));
+        Parent root = loader.load();
+
+        ServerViewController controller = loader.getController();
+        ServerControllerImpl serverController = new ServerControllerImpl();
+
+        serverController.setServerController(controller);
+        controller.setServerController(serverController);
+        controller.setStage(stage);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
